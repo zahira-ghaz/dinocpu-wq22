2f5d40ca878f88c3d960fe546223c135a227bad5
Zahira Ghazali <zmghazali@ucdavis.edugit config --global user.email zmghazali@ucdavis.edu>
2022-01-23 22:01:13 -0800

    adding

--- src/main/scala/single-cycle/cpu.scala
+++ src/main/scala/single-cycle/cpu.scala
@@ -25,8 +25,8 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   val (cycleCount, _) = Counter(true.B, 1 << 30)
 
   // Should be removed when wired are connected
-  immGen.io     := DontCare
-  nextpc.io     := DontCare
+  //immGen.io     := DontCare
+  //nextpc.io     := DontCare
   io.dmem       := DontCare
 
   //FETCH
@@ -34,7 +34,7 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   io.imem.valid := true.B
 
   val instruction = io.imem.instruction
-
+  //CONTROL
   control.io.opcode := instruction(6, 0)
 
   registers.io.readreg1 := instruction(19, 15)
@@ -42,25 +42,37 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   registers.io.writereg := instruction(11, 7)
   registers.io.writedata := alu.io.result
   when (registers.io.writereg =/= 0.U) {
-    registers.io.wen := true.B
+    registers.io.wen := control.io.wen //equal to write command from cpu if writereg is not equal to zero
   } .otherwise {
     registers.io.wen := false.B
   }
 
 
-  aluControl.io.aluop := true.B
-  aluControl.io.itype := false.B
-  aluControl.io.funct7 := instruction(31, 25)
-  aluControl.io.funct3 := instruction(14, 12)
+  aluControl.io.aluop := control.io.aluop
+  aluControl.io.itype := control.io.itype
+  aluControl.io.funct7 := instruction(31, 25) //FUNCT7
+  aluControl.io.funct3 := instruction(14, 12) //FUNCT3
   aluControl.io.wordinst := control.io.wordinst
 
   alu.io.operation := aluControl.io.operation
   alu.io.inputx := registers.io.readdata1
   alu.io.inputy := registers.io.readdata2
 
-  pcAdder.io.inputx := pc
-  pcAdder.io.inputy := 4.U
-  pc := pcAdder.io.result
+  //pcAdder.io.inputx := pc
+  //pcAdder.io.inputy := 4.U
+  //pc := pcAdder.io.result
+
+  nextpc.io.funct3 := instruction(14, 12)
+  nextpc.io.jumptype := control.io.jumpType
+  nextpc.io.inputx := registers.io.readdata1
+  nextpc.io.inputy := registers.io.readdata2
+  nextpc.io.pc := pc
+  nextpc.io.branch := false.B //for now
+  nextpc.io.imm := immGen.io.sextImm
+
+  immGen.io.instruction := io.imem.instruction
+
+  pc := nextpc.io.nextpc //connecting next pc to pc
 }
 
 /*
